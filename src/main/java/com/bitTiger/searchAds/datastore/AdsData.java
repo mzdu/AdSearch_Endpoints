26080d26e6890f173adc169c797a4f8784640373
mzdu <daniel_pub@hotmail.com>
2016-06-05 19:39:53 -0700

    added objectify to pom.xml

---
+++ src/main/java/com/bitTiger/searchAds/datastore/AdsData.java
@@ -0,0 +1,25 @@
+package com.bittiger.searchAds.datastore;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+
+@Entity
+public class AdsData {
+	
+	@Id Integer _adsId;
+	Integer _campaignId;
+	
+	public AdsData(Integer _adsId, Integer _campaignId){
+		this._adsId = _adsId;
+		this._campaignId = _campaignId;
+	};
+
+	public Integer getId() {
+		return _ads;
+	}
+	
+	public Integer getCampaignId() {
+		return _campaignId;
+	}
+	
+}
